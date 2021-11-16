cf11ac1e345e36f7ee653a108ec3f87d4023e123
Prof. H <profh@cmu.edu>
2021-11-16 08:47:50 -0500

    Tweaking challenge text

--- challenge_2.dart
+++ challenge_2.dart
@@ -1,7 +1,10 @@
 void main() {
   // Challenge 1: use a for-loop to print the square of each number from 1 to 10
 
-  // Challenge 2: print all the desserts that start with 'c' without a for-loop
+  // -----
+  // Challenge 2: print all the desserts that start with 'c' without a
+  // traditional for-loop
+
   List<String> desserts = ['cookies', 'cupcakes', 'donuts', 'pie', 'pudding'];
 
   //
